41fe81a01f7d22df5f4b7b49644fadc54fc5d7c8
unilock <unilock@fennet.rentals>
2024-06-07 17:39:15 -0400

    dynamic surroundings fabric...
    Signed-off-by: unilock <unilock@fennet.rentals>

---
+++ src/main/java/cc/unilock/shutup/mixin/dsurround/MusicTrackerMixinSquared.java
@@ -0,0 +1,20 @@
+package cc.unilock.shutup.mixin.dsurround;
+
+import com.bawnorton.mixinsquared.TargetHandler;
+import net.minecraft.client.sound.MusicTracker;
+import org.orecruncher.dsurround.lib.logging.IModLog;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(value = MusicTracker.class, priority = 1500)
+public class MusicTrackerMixinSquared {
+	@TargetHandler(
+		mixin = "org.orecruncher.dsurround.mixins.audio.MixinMusicManager",
+		name = "dsurround_startPlaying"
+	)
+	@Redirect(method = "@MixinSquared:Handler", at = @At(value = "INVOKE", target = "Lorg/orecruncher/dsurround/lib/logging/IModLog;info(Ljava/lang/String;[Ljava/lang/Object;)V"))
+	private static void dsurround_startPlaying(IModLog instance, String msg) {
+		// NO-OP
+	}
+}
