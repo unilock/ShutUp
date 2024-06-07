df3cd7158341c5f7a3c62c610d58027552a5b64a
unilock <unilock@fennet.rentals>
2024-06-07 14:50:08 -0400

    nbt autocomplete
    Signed-off-by: unilock <unilock@fennet.rentals>

---
+++ src/main/java/cc/unilock/shutup/mixin/nbt_ac/TypeLoaderMixin.java
@@ -0,0 +1,30 @@
+package cc.unilock.shutup.mixin.nbt_ac;
+
+import com.mt1006.nbt_ac.autocomplete.loader.typeloader.TypeLoader;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(value = TypeLoader.class, remap = false)
+public class TypeLoaderMixin {
+	@Redirect(method = "loadEntityTypes", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;error(Ljava/lang/String;)V"))
+	private static void loadEntityTypes$error(Logger instance, String msg) {
+		// NO-OP
+	}
+
+	@Redirect(method = "loadEntityTypes", at = @At(value = "INVOKE", target = "Lcom/mt1006/nbt_ac/autocomplete/loader/Loader;printStackTrace(Ljava/lang/Exception;)V"))
+	private static void loadEntityTypes$printStackTrace(Exception exception) {
+		// NO-OP
+	}
+
+	@Redirect(method = "loadBlockEntityTypes", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;error(Ljava/lang/String;)V"))
+	private static void loadBlockEntityTypes$error(Logger instance, String msg) {
+		// NO-OP
+	}
+
+	@Redirect(method = "loadBlockEntityTypes", at = @At(value = "INVOKE", target = "Lcom/mt1006/nbt_ac/autocomplete/loader/Loader;printStackTrace(Ljava/lang/Exception;)V"))
+	private static void loadBlockEntityTypes$printStackTrace(Exception exception) {
+		// NO-OP
+	}
+}
