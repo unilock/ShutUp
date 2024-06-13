0e30ed279909b423a98315b41b91afaea59e3f5c
unilock <unilock@fennet.rentals>
2024-06-13 19:02:54 -0400

    loot integrations
    Signed-off-by: unilock <unilock@fennet.rentals>

---
+++ src/main/java/cc/unilock/shutup/mixin/lootintegrations/LootModifierManagerMixin.java
@@ -0,0 +1,20 @@
+package cc.unilock.shutup.mixin.lootintegrations;
+
+import com.lootintegrations.loot.LootModifierManager;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(value = LootModifierManager.class, remap = false)
+public class LootModifierManagerMixin {
+	@Redirect(method = "apply(Ljava/util/Map;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)V", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;)V"))
+	private void warn(Logger instance, String s) {
+		// NO-OP
+	}
+
+	@Redirect(method = "apply(Ljava/util/Map;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)V", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;Ljava/lang/Throwable;)V"))
+	private void warn(Logger instance, String s, Throwable throwable) {
+		// NO-OP
+	}
+}
