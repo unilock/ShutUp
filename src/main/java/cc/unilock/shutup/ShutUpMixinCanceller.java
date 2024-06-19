e31e3bf9c011cba575a8ff1e776dbc9d7d8c7384
unilock <unilock@fennet.rentals>
2024-06-19 14:59:17 -0400

    cancel KawaiiDishes mixin that kills tps
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/shutup/ShutUpMixinCanceller.java
+++ src/main/java/cc/unilock/shutup/ShutUpMixinCanceller.java
@@ -5,8 +5,12 @@ import com.bawnorton.mixinsquared.api.MixinCanceller;
 import java.util.List;
 
 public class ShutUpMixinCanceller implements MixinCanceller {
+	private static final List<String> CANCELLED = List.of(
+		"com.hakimen.kawaiidishes.mixin.PacifyEntityMixin"
+	);
+
 	@Override
 	public boolean shouldCancel(List<String> targetClassNames, String mixinClassName) {
-		return false;
+		return CANCELLED.contains(mixinClassName);
 	}
 }
