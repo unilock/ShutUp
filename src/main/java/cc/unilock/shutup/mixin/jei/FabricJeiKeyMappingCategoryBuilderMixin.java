78a6951d096edd5154eb9ab5039638aae4de230c
unilock <unilock@fennet.rentals>
2024-09-18 15:32:44 -0400

    jei keybind workaround
    Signed-off-by: unilock <unilock@fennet.rentals>

---
+++ src/main/java/cc/unilock/shutup/mixin/jei/FabricJeiKeyMappingCategoryBuilderMixin.java
@@ -0,0 +1,15 @@
+package cc.unilock.shutup.mixin.jei;
+
+import mezz.jei.fabric.input.FabricJeiKeyMappingCategoryBuilder;
+import net.fabricmc.loader.api.FabricLoader;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(FabricJeiKeyMappingCategoryBuilder.class)
+public class FabricJeiKeyMappingCategoryBuilderMixin {
+	@Redirect(method = "createMapping", at = @At(value = "INVOKE", target = "Lnet/fabricmc/loader/api/FabricLoader;isModLoaded(Ljava/lang/String;)Z"))
+	private boolean isModLoaded(FabricLoader instance, String id) {
+		return false;
+	}
+}
