125715da5990662c7bc3870554d23f580f0156a4
unilock <katherines1337@outlook.com>
2025-11-05 08:28:17 -0500

    just keep amecs downgraded...

--- src/main/java/cc/unilock/shutup/mixin/lootintegrations/LootModifierManagerMixin.java
+++ src/main/java/cc/unilock/shutup/mixin/lootintegrations/LootModifierManagerMixin.java
@@ -8,12 +8,12 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(value = LootModifierManager.class, remap = false)
 public class LootModifierManagerMixin {
-	@Redirect(method = "apply(Ljava/util/Map;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)V", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;)V"))
+	@Redirect(method = "apply(Ljava/util/Map;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)V", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;)V"), remap = true)
 	private void warn(Logger instance, String s) {
 		// NO-OP
 	}
 
-	@Redirect(method = "apply(Ljava/util/Map;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)V", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;Ljava/lang/Throwable;)V"))
+	@Redirect(method = "apply(Ljava/util/Map;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)V", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;Ljava/lang/Throwable;)V"), remap = true)
 	private void warn(Logger instance, String s, Throwable throwable) {
 		// NO-OP
 	}
